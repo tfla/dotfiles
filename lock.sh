5168d113de5264c16c47df68eabb869042c6133d
Timmy Larsson <timmy.larsson@bodhilinux.com>
2016-10-29 19:51:24 +0200

    Added locking script.

---
+++ lock.sh
@@ -0,0 +1,6 @@
+TMPBG=$HOME/.screen.png
+LOCK=$HOME/.lock.png
+RES=$(xrandr | grep '*' | sed -E "s/[^0-9]*([0-9]+)x([0-9]+).*/\1*\2/")
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=10:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
+i3lock -u -i $TMPBG
+rm $TMPBG
